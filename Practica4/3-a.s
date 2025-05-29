517f5d62f9064dede4bb66f4c0ebffa8afdad38c
AureloSong <fnat.123@gmail.com>
2025-05-29 12:48:53 -0300

    changes

--- Practica4/3-a.s
+++ Practica4/3-a.s
@@ -8,10 +8,12 @@ DATA:    .word      0x10008
         daddi       $t2, $zero, ingreso
         sd          $t2, 0($t1)
         daddi       $t2, $zero, 4
-        sd          $t2, 0($t0)         ;leo nro 1
-        ld          $t2, $zero, 8       ;paso nro 1 a reg
-        sd          $t2, 0($t0)         ;leo nro 2
-        ld          $t4, 0($t1)         ;paso nro 2 a reg
+        sd          $t2, 0($t0)         
+        daddi       $t2, $zero, 8       
+        sd          $t2, 0($t0)
+        ld          $t3, 0($t1)
+        sd          $t2, 0($t0)
+        ld          $t4, 0($t1)       
         dadd        $t4, $t3, $t4       ;sumo numeros
         sd          $t4, 0($t1)
         daddi       $t2, $zero, 2
