ff56680e9b03e5d527eecad32dafce3571d5243d
AureloSong <fnat.123@gmail.com>
2025-06-03 09:17:11 -0300

    new files with examples.

--- winmips.s
+++ winmips.s
@@ -1,8 +1,29 @@
-            .data
-DATA:       0x10000
-CONTROL:    0x10008
-texto:      .asciiz "Ingrese una cadena. finaliza ingreso con 0"
-cont:       
-cadena:     .asciiz
-            .code
+            .data 
+CONTROL: .word 0x10000
+DATA:    .word 0x10008 
+msj:     .asciiz "Ingresa cada de caracteres: "
+cad1:    .asciiz ""
+cad2:    .asciiz ""
 
+            .code
+        ld $t6, CONTROL($zero)
+        ld $t7, DATA($zero)
+        daddi $t2, $zero, msj
+        sd $t2, 0($t7)
+        daddi $t2, $zero, 4
+        sd $t2, 0($t6)
+        ; mensaje impreso
+
+        daddi $t3, $zero, cad1
+        daddi $t2, $zero, 9
+
+loop:   sd $t2, 0($t6)
+        lbu $t4, 0($t7)
+        beqz $t4, es_Zero
+        sb $t5, 0($t3)
+        daddi $t3, $t3, 1
+        
+
+es_Zero:
+        
+        halt
